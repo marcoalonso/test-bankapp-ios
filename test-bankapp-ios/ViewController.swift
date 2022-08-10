2745166cbcdb1cdc2a92729a92bcc5b10a7a994e
Marco Alonso <marco@garagecoders.net>
2022-08-10 18:22:40 -0500

    Commit inicial

---
+++ test-bankapp-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-bankapp-ios
+//
+//  Created by marco rodriguez on 10/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
