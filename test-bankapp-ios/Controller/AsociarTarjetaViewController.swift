eab45ee92e9d261ce2fc82178428c403826c4ac2
Marco Alonso <marco@garagecoders.net>
2022-08-10 20:29:57 -0500

    UI Kit

---
+++ test-bankapp-ios/Controller/AsociarTarjetaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AsociarTarjetaViewController.swift
+//  test-bankapp-ios
+//
+//  Created by marco rodriguez on 10/08/22.
+//
+
+import UIKit
+
+class AsociarTarjetaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ test-bankapp-ios/View/CustomCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCell.swift
+//  test-bankapp-ios
+//
+//  Created by marco rodriguez on 10/08/22.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+}
