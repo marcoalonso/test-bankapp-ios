bb5a3162c25ca4ab23b3c175e668422db8c48d31
Marco Alonso <marco@garagecoders.net>
2022-08-10 21:31:30 -0500

    Maqueta dashboard p1

--- test-bankapp-ios/Controller/TarjetasViewController.swift
+++ test-bankapp-ios/Controller/TarjetasViewController.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class TarjetasViewController: UIViewController {
-
+    @IBOutlet weak var MisCuentasButton: UIButton!
+    @IBOutlet weak var EnviarDineroButton: UIButton!
+    @IBOutlet weak var agregarTarjetaButton: UIButton!
+    @IBOutlet weak var saldosCollectionView: UICollectionView!
+    
+    // MARK: - Variables
+    let subrayadoButton: [NSAttributedString.Key: Any] = [.underlineStyle: NSUnderlineStyle.single.rawValue]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        saldosCollectionView.delegate = self
+        saldosCollectionView.dataSource = self
+        saldosCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureUI(){
+        
+        //collectionView
+        if let layout = saldosCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+        
+        //ButtonStyle
+        let attributeStringCuentas = NSMutableAttributedString(string: "Mis Cuentas", attributes: subrayadoButton)
+        let attributeStringEnviar = NSMutableAttributedString(string: "Enviar Dinero", attributes: subrayadoButton)
+        let attributeStringAgregar = NSMutableAttributedString(string: "+ Agregar una tarjeta de débito o crédito", attributes: subrayadoButton)
+        MisCuentasButton.setAttributedTitle(attributeStringCuentas, for: .normal)
+        EnviarDineroButton.setAttributedTitle(attributeStringEnviar, for: .normal)
+        agregarTarjetaButton.setAttributedTitle(attributeStringAgregar, for: .normal)
     }
-    */
 
+}
+
+extension TarjetasViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celda = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        
+        return celda
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 243, height: 125)
+    }
+    
 }
