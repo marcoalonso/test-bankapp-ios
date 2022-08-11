738146e03a2a6e52a42eac0a13df142ecb93ac85
Marco Alonso <marco@garagecoders.net>
2022-08-11 00:58:20 -0500

    Maqueta form view

--- test-bankapp-ios/Controller/AsociarTarjetaViewController.swift
+++ test-bankapp-ios/Controller/AsociarTarjetaViewController.swift
@@ -15,15 +15,9 @@ class AsociarTarjetaViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: true)
     }
-    */
+    
 
 }
